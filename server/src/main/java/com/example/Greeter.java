c13da3e1b57491f8af918db81c00517cf9d42494
root <root@localhost.localdomain>
2018-09-17 12:25:23 +0530

    Check Style warnings Fixed 1

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
+
 /**
- * class
+ * @param someone the name of person
+ * @return greeting string
  */
   public final  String greet(final String someone) {
     return String.format("Hello, %s!", someone);
